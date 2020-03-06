f4c82c05db00febc9d7cc94f210dd580843cec75
ken-kun <kevin.ly96@live.ca>
2020-03-06 09:38:25 -0500

    Created Initial Project

---
+++ Furry'sAerialFury/Furry'sAerialFuryTests/Furry_sAerialFuryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Furry_sAerialFuryTests.swift
+//  Furry'sAerialFuryTests
+//
+//  Created by graphic on 2020-03-06.
+//  Copyright © 2020 graphic. All rights reserved.
+//
+
+import XCTest
+@testable import Furry_sAerialFury
+
+class Furry_sAerialFuryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
