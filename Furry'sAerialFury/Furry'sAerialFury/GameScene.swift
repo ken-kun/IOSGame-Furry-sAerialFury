ec034bb5790c8cad221b871a78fc8a460f6b4f4b
ken-kun <kevin.ly2@georgebrown.ca>
2020-04-10 15:48:38 -0400

    Multiple updates. please review

--- Furry'sAerialFury/Furry'sAerialFury/GameScene.swift
+++ Furry'sAerialFury/Furry'sAerialFury/GameScene.swift
@@ -8,6 +8,7 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 struct PhysicsBodyType {
     static let rabbitPlane: UInt32 = 0x1 << 1
@@ -40,15 +41,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var TextureAtlasEnemy = SKTextureAtlas()
     var TextureArrayEnemy = [SKTexture]()
     
+    var TextureAtlasEExplosion = SKTextureAtlas()
+    var TextureArrayEEnemy = [SKTexture]()
+    var TextureAtlasPExplosion = SKTextureAtlas()
+    var TextureArrayEPlayer = [SKTexture]()
+    
+    
     var background = SKSpriteNode()
     var rabbitPlane = SKSpriteNode()
     var enemyPlane = SKSpriteNode()
     var enemyGroup = SKNode()
     var moveAndRemove = SKAction()
     
+    var score = Int()
+    let scoreLabel = SKLabelNode()
     
+    var death = Bool()
+    var gameStart = Bool()
+    var mainGame = SKLabelNode()
     
-    
+    var BGMaudio = AVAudioPlayer()
+    var enemyExplosion = SKSpriteNode()
+    var playerExplosion = SKSpriteNode()
     
     
     
@@ -71,147 +85,228 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
       
     }
     override func didMove(to view: SKView) {
+        
+        physicsWorld.contactDelegate = self
+        
         setupJoystick()
-        self.backgroundColor = SKColor.init(red: 0, green: 0, blue: 0, alpha: 0)
-       let mainGame = SKLabelNode(fontNamed: "Chalkduster")
-        mainGame.text = "Main Game!"
-        mainGame.fontSize = 45
-        mainGame.position = CGPoint(x: frame.midX, y: frame.midY+300)
-        addChild(mainGame)
         
-        let scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-        scoreLabel.text = "Score: "
-        scoreLabel.fontSize = 30
-        scoreLabel.position = CGPoint(x: frame.midX+80, y: frame.midY+350)
-        addChild(scoreLabel)
         
-        pauseButton.frame = CGRect(x: 20.0, y: 50.0, width: 70.0, height: 50.0)
-        pauseButton.setTitle("Pause", for: .normal)
-        pauseButton.layer.borderWidth = 5.0
-        pauseButton.layer.borderColor = UIColor.white.cgColor
-        pauseButton.backgroundColor = UIColor.black
-        pauseButton.titleLabel?.textColor = UIColor.white
-        pauseButton.tintColor = UIColor.white
-        pauseButton.layer.cornerRadius = 15.0
-        pauseButton.addTarget(self, action: #selector(pauseButtonAction(_:)), for: .touchUpInside)
-        self.view?.addSubview(pauseButton)
+         self.backgroundColor = SKColor.init(red: 0, green: 0, blue: 0, alpha: 0)
+         mainGame = SKLabelNode(fontNamed: "Chalkduster")
+         mainGame.text = "Press the screen to Start!"
+         mainGame.zPosition = 3
+         mainGame.fontSize = 20
+         mainGame.position = CGPoint(x: frame.midX, y: frame.midY+300)
+         addChild(mainGame)
+         
+         let scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+         scoreLabel.text = "Score: " + "\(score)"
+         scoreLabel.fontSize = 30
+         scoreLabel.zPosition = 3
+         scoreLabel.position = CGPoint(x: frame.midX+80, y: frame.midY+350)
+         self.addChild(scoreLabel)
+         
+         pauseButton.frame = CGRect(x: 20.0, y: 50.0, width: 70.0, height: 50.0)
+         pauseButton.setTitle("Pause", for: .normal)
+         pauseButton.layer.borderWidth = 5.0
+         pauseButton.layer.borderColor = UIColor.white.cgColor
+         pauseButton.backgroundColor = UIColor.black
+         pauseButton.titleLabel?.textColor = UIColor.white
+         pauseButton.tintColor = UIColor.white
+         pauseButton.layer.cornerRadius = 15.0
+         pauseButton.addTarget(self, action: #selector(pauseButtonAction(_:)), for: .touchUpInside)
+         self.view?.addSubview(pauseButton)
+         
+         attackButton.frame = CGRect(x: 270.0, y: 700.0, width: 70.0, height: 70.0)
+         attackButton.setTitle("A", for: .normal)
+         attackButton.layer.borderWidth = 5.0
+         attackButton.layer.borderColor = UIColor.white.cgColor
+         attackButton.backgroundColor = UIColor.black
+         attackButton.titleLabel?.textColor = UIColor.white
+         attackButton.tintColor = UIColor.white
+         attackButton.layer.cornerRadius = attackButton.frame.size.width / 2
+         attackButton.layer.masksToBounds = true
+         attackButton.addTarget(self, action: #selector(attackButtonAction(_:)), for: .touchUpInside)
+         self.view?.addSubview(attackButton)
+         
+         specialButton.frame = CGRect(x: 320.0, y: 650.0, width: 50.0, height: 50.0)
+         specialButton.setTitle("S", for: .normal)
+         specialButton.layer.borderWidth = 5.0
+         specialButton.layer.borderColor = UIColor.white.cgColor
+         specialButton.backgroundColor = UIColor.black
+         specialButton.titleLabel?.textColor = UIColor.white
+         specialButton.tintColor = UIColor.white
+         specialButton.layer.cornerRadius = specialButton.frame.size.width / 2
+         specialButton.layer.masksToBounds = true
+         specialButton.addTarget(self, action: #selector(specialButtonAction(_:)), for: .touchUpInside)
+         self.view?.addSubview(specialButton)
+         /*
+         DPAD.frame = CGRect(x: 50.0, y: 680.0, width: 100.0, height: 100.0)
+         DPAD.setTitle("D-PAD", for: .normal)
+         DPAD.layer.borderWidth = 5.0
+         DPAD.layer.borderColor = UIColor.white.cgColor
+         DPAD.backgroundColor = UIColor.black
+         DPAD.titleLabel?.textColor = UIColor.white
+         DPAD.tintColor = UIColor.white
+         DPAD.layer.cornerRadius = DPAD.frame.size.width / 2
+         DPAD.layer.masksToBounds = true
+         DPAD.addTarget(self, action: #selector(specialButtonAction(_:)), for: .touchUpInside)
+         self.view?.addSubview(DPAD)
+         */
+         /////////////////////////////////////////////////////////////
+         
+         for i in 0..<2 {
+             background = SKSpriteNode(imageNamed: "gamebackground.png")
+             background.anchorPoint = CGPoint(x: 0, y: 0)
+             background.setScale(1)
+             background.position = CGPoint(x: 0, y: CGFloat(i) * self.frame.height)
+             background.name = "background"
+             self.addChild(background)
+             
+         }
         
-        attackButton.frame = CGRect(x: 270.0, y: 700.0, width: 70.0, height: 70.0)
-        attackButton.setTitle("A", for: .normal)
-        attackButton.layer.borderWidth = 5.0
-        attackButton.layer.borderColor = UIColor.white.cgColor
-        attackButton.backgroundColor = UIColor.black
-        attackButton.titleLabel?.textColor = UIColor.white
-        attackButton.tintColor = UIColor.white
-        attackButton.layer.cornerRadius = attackButton.frame.size.width / 2
-        attackButton.layer.masksToBounds = true
-        attackButton.addTarget(self, action: #selector(attackButtonAction(_:)), for: .touchUpInside)
-        self.view?.addSubview(attackButton)
+        let BGM = Bundle.main.path(forResource: "Floral Fury", ofType: "mp3")
         
-        specialButton.frame = CGRect(x: 320.0, y: 650.0, width: 50.0, height: 50.0)
-        specialButton.setTitle("S", for: .normal)
-        specialButton.layer.borderWidth = 5.0
-        specialButton.layer.borderColor = UIColor.white.cgColor
-        specialButton.backgroundColor = UIColor.black
-        specialButton.titleLabel?.textColor = UIColor.white
-        specialButton.tintColor = UIColor.white
-        specialButton.layer.cornerRadius = specialButton.frame.size.width / 2
-        specialButton.layer.masksToBounds = true
-        specialButton.addTarget(self, action: #selector(specialButtonAction(_:)), for: .touchUpInside)
-        self.view?.addSubview(specialButton)
-        /*
-        DPAD.frame = CGRect(x: 50.0, y: 680.0, width: 100.0, height: 100.0)
-        DPAD.setTitle("D-PAD", for: .normal)
-        DPAD.layer.borderWidth = 5.0
-        DPAD.layer.borderColor = UIColor.white.cgColor
-        DPAD.backgroundColor = UIColor.black
-        DPAD.titleLabel?.textColor = UIColor.white
-        DPAD.tintColor = UIColor.white
-        DPAD.layer.cornerRadius = DPAD.frame.size.width / 2
-        DPAD.layer.masksToBounds = true
-        DPAD.addTarget(self, action: #selector(specialButtonAction(_:)), for: .touchUpInside)
-        self.view?.addSubview(DPAD)
-        */
-        /////////////////////////////////////////////////////////////
+        do {
+            BGMaudio = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: BGM!))
+        }
+        catch{
+            print(error)
+        }
         
-        for i in 0..<2 {
-            background = SKSpriteNode(imageNamed: "gamebackground.png")
-            background.anchorPoint = CGPoint(x: 0, y: 0)
-            background.setScale(1)
-            background.position = CGPoint(x: 0, y: CGFloat(i) * self.frame.height)
-            background.name = "background"
-            self.addChild(background)
+        
+        BGMaudio.play()
+        
+        
+        
+        createScene()
+
+        
+    }
+    
+    func restartScene(){
+        self.removeAllChildren()
+        self.removeAllActions()
+        score = 0
+        death = false
+        gameStart = false
+    }
+    
+    func createScene(){
+        
+        physicsWorld.contactDelegate = self
+        
+         
+         TextureAtlas = SKTextureAtlas(named: "plane.atlas")
+             for i in 0...1{
+                 
+                 let Name = "Plane\(i)"
+                 TextureArray.append(SKTexture(imageNamed: Name))
+                 
+             }
+             rabbitPlane = SKSpriteNode(imageNamed: "Plane0" )
+             rabbitPlane.setScale(0.5)
+             rabbitPlane.position = CGPoint(x:self.frame.width/2, y: self.frame.height/2 - 200)
+             rabbitPlane.zPosition = 3
+             rabbitPlane.physicsBody = SKPhysicsBody(rectangleOf: rabbitPlane.size )
+             rabbitPlane.physicsBody?.categoryBitMask = PhysicsBodyType.rabbitPlane
+             rabbitPlane.physicsBody?.collisionBitMask = PhysicsBodyType.enemyBullet | PhysicsBodyType.enemyPlane
+             rabbitPlane.physicsBody?.contactTestBitMask = PhysicsBodyType.enemyBullet | PhysicsBodyType.enemyPlane
+             rabbitPlane.physicsBody?.affectedByGravity = false
+             rabbitPlane.physicsBody?.isDynamic = true;
+             rabbitPlane.run(SKAction.repeatForever(SKAction.animate(with: TextureArray, timePerFrame: 0.1, resize: true, restore: true)))
+         
+             self.addChild(rabbitPlane)
+         
+         
+         
+         
+         
+         
+         
+         
+         
+
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+        
+        if(firstBody.categoryBitMask == PhysicsBodyType.enemyPlane && secondBody.categoryBitMask == PhysicsBodyType.rabbitPlane || firstBody.categoryBitMask == PhysicsBodyType.rabbitPlane && secondBody.categoryBitMask == PhysicsBodyType.enemyPlane)
+        {
+            //score+=1
+            //scoreLabel.text = "\(score)"
+            //print("Bodies hit")
             
+            enemyBoom()
+            playerBoom()
+            
+            
+        }
+        
+        if(firstBody.categoryBitMask == PhysicsBodyType.enemyPlane && secondBody.categoryBitMask == PhysicsBodyType.bullet || firstBody.categoryBitMask == PhysicsBodyType.bullet && secondBody.categoryBitMask == PhysicsBodyType.enemyPlane)
+        {
+            score+=1
+            scoreLabel.text = "Score: " + "\(score)"
+            scoreLabel.zPosition = 3
+            print("Player Bulley hit")
+            print(score)
+            
+            enemyBoom()
+            
+            
+            
+            
+            
+        }
+        
+        if(firstBody.categoryBitMask == PhysicsBodyType.rabbitPlane && secondBody.categoryBitMask == PhysicsBodyType.enemyBullet || firstBody.categoryBitMask == PhysicsBodyType.enemyBullet && secondBody.categoryBitMask == PhysicsBodyType.rabbitPlane)
+        {
+            //isPaused = true
+            //print("Enemy Bullet hit")
+            
+            playerBoom()
         }
         
         
         
+    }
+    
+    
+    func enemyBoom(){
         
-        TextureAtlas = SKTextureAtlas(named: "plane.atlas")
-            for i in 0...1{
-                
-                let Name = "Plane\(i)"
-                TextureArray.append(SKTexture(imageNamed: Name))
-                
-            }
-            rabbitPlane = SKSpriteNode(imageNamed: "Plane0" )
-            rabbitPlane.setScale(0.5)
-            rabbitPlane.position = CGPoint(x:self.frame.width/2, y: self.frame.height/2 - 200)
-            rabbitPlane.zPosition = 3
-            rabbitPlane.physicsBody = SKPhysicsBody(rectangleOf: rabbitPlane.size )
-            rabbitPlane.physicsBody?.categoryBitMask = PhysicsBodyType.rabbitPlane
-            rabbitPlane.physicsBody?.collisionBitMask = PhysicsBodyType.enemyBullet | PhysicsBodyType.enemyPlane
-            rabbitPlane.physicsBody?.contactTestBitMask = PhysicsBodyType.enemyBullet | PhysicsBodyType.enemyPlane
-            rabbitPlane.physicsBody?.affectedByGravity = false
-            rabbitPlane.physicsBody?.isDynamic = true;
-            rabbitPlane.run(SKAction.repeatForever(SKAction.animate(with: TextureArray, timePerFrame: 0.1, resize: true, restore: true)))
+        enemyExplosion = SKSpriteNode(imageNamed: "Explotion1.png")
+        enemyExplosion.setScale(1)
+        enemyExplosion.zPosition = 3
+        enemyExplosion.position = CGPoint(x: enemyPlane.position.x, y: enemyPlane.position.y)
         
-            self.addChild(rabbitPlane)
-        
-        
-        
-        let spawn = SKAction.run({
-            () in
+        TextureAtlasEExplosion = SKTextureAtlas(named: "EnemyExplosion.atlas")
+        for i in 1...3{
             
+            let Name = "Explotion\(i)"
+            TextureArrayEEnemy.append(SKTexture(imageNamed: Name))
             
-            self.enemySpawn()
-        })
+        }
         
-        let delay = SKAction.wait(forDuration: 2.0)
-        let spawnDelay = SKAction.sequence([spawn, delay])
-        let spawnDelayForever = SKAction.repeatForever(spawnDelay)
-        self.run(spawnDelayForever)
+        enemyPlane.run(SKAction.animate(with: TextureArrayEEnemy, timePerFrame: 0.1, resize: true, restore: true))
+    }
+    
+    func playerBoom(){
+        playerExplosion = SKSpriteNode(imageNamed: "Explotion4.png")
+        playerExplosion.setScale(1)
+        playerExplosion.zPosition = 3
+        playerExplosion.position = CGPoint(x: rabbitPlane.position.x, y: rabbitPlane.position.y)
         
-        let distance = CGFloat(self.frame.width * 3 + enemyPlane.frame.width)
-        let moveEnemy = SKAction.moveBy(x: 0, y: -distance - 60, duration: TimeInterval(0.010 * distance ))
-        let removeEnemy = SKAction.removeFromParent()
-        moveAndRemove = SKAction.sequence([moveEnemy, removeEnemy])
-        
-        
-        let Bulletspawn = SKAction.run({
-            () in
+        TextureAtlasPExplosion = SKTextureAtlas(named: "PlayerExplosion.atlas")
+        for i in 4...8{
             
-            self.enemyFireBullet()
+            let Name = "Explotion\(i)"
+            TextureArrayEPlayer.append(SKTexture(imageNamed: Name))
             
-        })
+        }
         
-        let timeDelay = SKAction.wait(forDuration: 2.0)
-        let spawnBulletDelay = SKAction.sequence([Bulletspawn, timeDelay])
-        let spawnForever = SKAction.repeatForever(spawnBulletDelay)
-        self.run(spawnForever)
-        
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        rabbitPlane.run(SKAction.animate(with: TextureArrayEPlayer, timePerFrame: 0.1, resize: true, restore: true))
     }
     
     func enemyFireBullet(){
@@ -220,7 +315,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let enemyBullet = SKSpriteNode(imageNamed: "EnemyBullet")
         enemyBullet.position = enemyPlane.position
         enemyBullet.position.y += 5
-        enemyBullet.zPosition = 2
+        enemyBullet.zPosition = 3
         
         enemyBullet.physicsBody = SKPhysicsBody(rectangleOf: enemyBullet.size)
         enemyBullet.physicsBody?.categoryBitMask = PhysicsBodyType.enemyBullet
@@ -254,7 +349,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let bullet = SKSpriteNode(imageNamed: "PlayerBullet")
         bullet.position = rabbitPlane.position
         bullet.position.y += 5
-        bullet.zPosition = 2
+        bullet.zPosition = 3
         
         bullet.physicsBody = SKPhysicsBody(rectangleOf: bullet.size)
         bullet.physicsBody?.categoryBitMask = PhysicsBodyType.bullet
@@ -294,7 +389,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             enemyPlane = SKSpriteNode(imageNamed: "Enemy0" )
             enemyPlane.setScale(0.5)
             enemyPlane.position = CGPoint(x:self.frame.width/2, y: self.frame.height/2 + 600)
-            enemyPlane.zPosition = 2
+            enemyPlane.zPosition = 3
         
             enemyPlane.physicsBody = SKPhysicsBody(rectangleOf: enemyPlane.size )
             enemyPlane.physicsBody?.categoryBitMask = PhysicsBodyType.enemyPlane
@@ -326,7 +421,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //DPAD.removeFromSuperview()
     }
     @objc func pauseButtonAction(_ : UIButton){
-       
+       isPaused = true
    }
     @objc func attackButtonAction(_ : UIButton){
         fireBullet()
@@ -334,10 +429,49 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
       @objc func specialButtonAction(_ : UIButton){
           
       }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         scene?.view?.presentScene(OptionScene(size: self.frame.size))
         
         //enemyFireBullet()
+        
+        mainGame.text = " "
+        
+        if(gameStart) == false {
+            
+            gameStart = true
+            
+            let spawn = SKAction.run({
+                () in
+                
+                
+                self.enemySpawn()
+            })
+            
+            let delay = SKAction.wait(forDuration: 2.0)
+            let spawnDelay = SKAction.sequence([spawn, delay])
+            let spawnDelayForever = SKAction.repeatForever(spawnDelay)
+            self.run(spawnDelayForever)
+            
+            let distance = CGFloat(self.frame.width * 3 + enemyPlane.frame.width)
+            let moveEnemy = SKAction.moveBy(x: 0, y: -distance - 60, duration: TimeInterval(0.010 * distance ))
+            let removeEnemy = SKAction.removeFromParent()
+            moveAndRemove = SKAction.sequence([moveEnemy, removeEnemy])
+            
+            
+            let Bulletspawn = SKAction.run({
+                () in
+                
+                self.enemyFireBullet()
+                
+            })
+            
+            let timeDelay = SKAction.wait(forDuration: 2.0)
+            let spawnBulletDelay = SKAction.sequence([Bulletspawn, timeDelay])
+            let spawnForever = SKAction.repeatForever(spawnBulletDelay)
+            self.run(spawnForever)
+            
+        }
         
         
         
@@ -359,4 +493,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
         }))
     }
+    
+    
+   
 }
